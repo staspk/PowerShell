d54f13a2cbed22f839d3cbbbb34569f947a646e4
Stanislav Kozubenko <staspk@gmail.com>
2024-12-06 12:12:51 -0800

    Save Profile Script Added

---
+++ saveProfile5.1.ps1
@@ -0,0 +1,11 @@
+# Deletes the contents of: .\WindowsPowerShell, and copy pastes everything under the WindowsPowerShell $profile to ScriptPackage
+
+$profileDir = "$env:USERPROFILE\Documents\WindowsPowerShell\"
+$scriptDir = ".\WindowsPowerShell"
+
+Get-ChildItem -Path $scriptDir -Recurse | ForEach-Object {
+    $_.Delete()
+    Write-Host "Deleted File: $_"
+}
+
+Copy-Item -Path "$profileDir\*" -Destination $scriptDir -Recurse
