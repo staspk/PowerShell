715c4d15140206fee51f7f9957fff74c6239d86c
Stanislav Kozubenko <staspk@gmail.com>
2024-12-13 01:38:57 -0800

    "Flattened script-files into single script-file"

--- saveProfilesAndPushToRemote.ps1
+++
@@ -1,5 +0,0 @@
-.\saveProfile5.1.ps1
-.\saveProfile7.4.ps1
-
-$commitMsg = Read-Host "Enter Commit Message"
-Push $commitMsg
---
+++ saveProfilesPushToRemote.ps1
@@ -0,0 +1,30 @@
+#  1) Deletes contents and copy-pastes into: .\WindowsPowerShell, from: $Env:userprofile\Documents\WindowsPowerShell
+#  2) Deletes contents and copy-pastes into: .\Powershell,        from: $Env:userprofile\Documents\WindowsPowerShell
+
+function DeleteContentsThenCopyPasteFromTo($profileDir, $scriptDir) {
+    Get-ChildItem -Path $scriptDir -Recurse | ForEach-Object {
+        $_.Delete()
+        Write-Host "Deleted File: $_" -ForegroundColor Red
+        Start-Sleep -Milliseconds 10
+    }
+    
+    Copy-Item -Path "$profileDir\*" -Destination $scriptDir -Recurse
+    Write-Host "`$Profile Contents COPY-PASTED to: $scriptDir" -ForegroundColor Green
+    Start-Sleep -Milliseconds 800
+    
+    
+    for ($i = 1; $i -le 5; $i++) {
+        Start-Sleep -Milliseconds 5
+        [console]::SetCursorPosition(0, [console]::CursorTop - 1)
+        Write-Host (" " * [console]::WindowWidth)
+        [console]::SetCursorPosition(0, [console]::CursorTop - 1)
+    }
+}
+
+DeleteContentsThenCopyPasteFromTo "$env:USERPROFILE\Documents\WindowsPowerShell" ".\WindowsPowerShell"
+DeleteContentsThenCopyPasteFromTo "$env:USERPROFILE\Documents\Powershell" ".\Powershell"
+$commitMessage = Read-Host "Enter Commit Message"
+
+git add .
+git commit -a -m $commitMessage
+git push
