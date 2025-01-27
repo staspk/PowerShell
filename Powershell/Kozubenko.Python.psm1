a09053e53e237fff889a2bd45b2c668025a7a164
Stanislav Kozubenko <staspk@gmail.com>
2025-01-26 21:02:19 -0800

    Automatic Push

--- Powershell/Kozubenko.Python.psm1
+++ Powershell/Kozubenko.Python.psm1
@@ -5,7 +5,7 @@ function Activate {     # Use from a Python project root dir, to activate a venv
     if (TestPathSilently "$PWD\venv")     {  Invoke-Expression "$PWD\venv\Scripts\Activate.ps1";    $global:venvActive = $true   }
 }
 
-function Freeze {
+function venvFreeze {
     if ($global:venvActive -and (TestPathSilently "$PWD\.venv" -or TestPathSilently "$PWD\venv")) {
         pip freeze > requirements.txt
         WriteCyan "Frozen: $PWD\requirements.txt"
@@ -13,4 +13,17 @@ function Freeze {
     else {
         WriteRed "`$venvActive == False"
     }
+}
+function venvInstallRequirements {
+    if ($global:venvActive -and (TestPathSilently "$PWD\.venv" -or TestPathSilently "$PWD\venv")) {
+        py -m pip install -r requirements.txt
+        WriteCyan "requirements.txt installed"
+    }
+    else {
+        WriteRed "`$venvActive == False"
+    }
+}
+
+function KillPythonProcesses {
+    Get-Process -Name python | Stop-Process -Force
 }
