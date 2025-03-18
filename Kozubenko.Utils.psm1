15e07e40709a75300d1c1b2c6170399e1d52cc85
Stanislav Kozubenko <staspk@gmail.com>
2025-03-17 20:16:43 -0700

    Print now calls all Write-Host funcs

--- Kozubenko.Utils.psm1
+++ Kozubenko.Utils.psm1
@@ -5,7 +5,7 @@ $LiteRed = $PSStyle.Foreground.FromRgb(223, 96, 107);
 
 function Print($text, $newLine = $true)         {  if($newLine) { Write-Host $text }         else {  Write-Host $text - -NoNewline  }  }
 function PrintWhiteRed($text, $newLine = $true) {  if($newLine) { Write-Host ${WhiteRed}$text }       else { Write-Host ${WhiteRed}$text -NoNewline }  }
-function PrintWhiteRed($text, $newLine = $true)  {  if($newLine) { Write-Host ${LiteRed}$text -ForegroundColor Red }      else { Write-Host ${LiteRed}$text -ForegroundColor Red -NoNewline }  }
+function PrintLiteRed($text, $newLine = $true)  {  if($newLine) { Write-Host ${LiteRed}$text -ForegroundColor Red }      else { Write-Host ${LiteRed}$text -ForegroundColor Red -NoNewline }  }
 function PrintRed($text, $newLine = $true)      {  if($newLine) { Write-Host $text -ForegroundColor Red }      else { Write-Host $text -ForegroundColor Red -NoNewline }        }
 function PrintDarkRed($text, $newLine = $true)  {  if($newLine) { Write-Host $text -ForegroundColor DarkRed }   else { Write-Host $text -ForegroundColor DarkRed -NoNewline }    }
 function PrintYellow($text, $newLine = $true)   {  if($newLine) { Write-Host $text -ForegroundColor Yellow }    else { Write-Host $text -ForegroundColor Yellow -NoNewline }     }
