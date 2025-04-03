6c389f692e59f4cae023bfae2c90733321072843
Stanislav Kozubenko <staspk@gmail.com>
2025-04-02 20:03:14 -0700

    +RestoreClassicContextMenu($reverse)

--- Kozubenko.Utils.psm1
+++ Kozubenko.Utils.psm1
@@ -106,6 +106,18 @@ function TurnOffSleepSettings([int]$time_in_hours = 0) {
     }
 }
 
+function RestoreClassicContextMenu([bool]$reverse = $false) {
+	$guid = "{86CA1AA0-34AA-4E8B-A509-50C905BAE2A2}" 
+	if(-not($reverse)) {
+		New-Item -Path "HKCU:\Software\Classes\CLSID\" 		-Name $guid 					| Out-Null
+		New-Item -Path "HKCU:\Software\Classes\CLSID\$guid" -Name InprocServer32 -Value "" 	| Out-Null
+	}
+	else {
+		Remove-Item -Path "HKCU:\Software\Classes\CLSID\$guid" -Recurse -Force -ErrorAction SilentlyContinue
+	}
+    Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
+}
+
 function ClearTerminal {
     if(ConsoleInputTextLength gt 0) {
         ConsoleDeleteInput
