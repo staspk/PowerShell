84b7df1efdd0b2bfc2cff78297ec89cd4b663d89
Stanislav Kozubenko <staspk@gmail.com>
2025-06-28 23:53:49 -0700

    redundant function removed: SetVar()

--- Kozubenko.Runtime.psm1
+++ Kozubenko.Runtime.psm1
@@ -4,7 +4,6 @@ using module .\Kozubenko.Utils.psm1
 # Since aliases can't have params, we have to use functions to accomplish this...
 function SetStartLocation($path = $PWD.Path)  {  $global:MyRuntime.SetStartLocation($path)  }
 function NewVar($name, $value = $PWD.Path)    {  $global:MyRuntime.NewVar($name, $value)  }
-function SetVar($name, $value)                {  $global:MyRuntime.SetVar($name, $value)  }
 function DeleteVar($varName)                  {  $global:MyRuntime.DeleteVar($varName)  }
 
 class MyRuntime {
@@ -16,11 +15,12 @@ class MyRuntime {
     MyRuntime([string]$pathToGlobals) {
         $this.PATH_TO_GLOBALS = $pathToGlobals;
         $this.modules = [System.Collections.Generic.List[FunctionRegistry]]::new();
-        $this.AddModule([FunctionRegistry]::new("Kozubenko.MyRuntime", @(
-            "SetStartLocation(`$path = `$PWD.Path)",
-            "NewVar(`$name, `$value = `$PWD.Path)",
-            "SetVar(`$name, `$value)",
-            "DeleteVar(`$varName)"))
+        $this.AddModule([FunctionRegistry]::new(
+            "Kozubenko.MyRuntime", @(
+                "SetStartLocation(`$path = `$PWD.Path)",
+                "NewVar(`$name, `$value = `$PWD.Path)",
+                "SetVar(`$name, `$value)",
+                "DeleteVar(`$varName)"))
         );
 
         if(-not(Test-Path $this.PATH_TO_GLOBALS)) {
@@ -36,32 +36,25 @@ class MyRuntime {
     }
 
     [void] AddModules([Array]$functionRegistrys) {
-        foreach ($funcReg in $functionRegistrys) {
-            $this.AddModule($funcReg)
+        foreach ($functionRegistry in $functionRegistrys) {
+            $this.modules.Add($functionRegistry)
         }
     }
     
-    SetStartLocation($path) {   # PUBLIC
+    SetStartLocation($path) {                                   <# GLOBAL PUBLIC FUNCTION #>
         $this.SaveToGlobals($this.START_LOCATION_KEY, $path)
         $this.LoadInGlobals($null)
         Set-Location $global:startlocation
     }
 
-    NewVar($name, $value) {   # PUBLIC
+    NewVar($name, $value) {                                     <# GLOBAL PUBLIC FUNCTION #>
         AssertString $name "name"
         if ($name[0] -eq "$") {  $name = $name.Substring(1, $name.Length - 1 )  }
         $this.SaveToGlobals($name, $value)
         $this.LoadInGlobals($null)
     }
 
-    SetVar($name, $value) {   # PUBLIC
-        AssertString $name "name"; AssertString $value "value"
-        if ($name[0] -eq "$") {  $name = $name.Substring(1, $name.Length - 1 )  }
-        $this.SaveToGlobals($name, $value)
-        $this.LoadInGlobals($null)
-    }
-
-    DeleteVar($varName) {    # PUBLIC
+    DeleteVar($varName) {                                       <# GLOBAL PUBLIC FUNCTION #>
         Clear-Host;
         if($varName[0] -eq "$") {  $varName = $varName.Substring(1)  }
         $this.LoadInGlobals($varName)
@@ -77,7 +70,7 @@ class MyRuntime {
             $openedTo -ieq "C:\Users\stasp\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"    # Powershell started with .ahk hotkey
         ) {
             if(IsDirectory $global:startLocation) {  Set-Location $global:startLocation }
-            elseif(IsFile $global:startLocation)  {  Set-Location $(ParentDir $global:startLocation)  }  # QoL, so it's easy set $profile as startupLocation
+            elseif(IsFile $global:startLocation)  {  Set-Location $(ParentDir $global:startLocation)  }      # QoL, so it's easy set $profile as startupLocation
             else {
                 PrintRed "`$startLocation path does not exist anymore. Defaulting to userdirectory..."
                 Set-Location $Env:USERPROFILE
@@ -85,16 +78,17 @@ class MyRuntime {
         }
     }
 
-    hidden [void] LoadInGlobals($varToDelete) {      # Cleanup while loading-in, e.g. duplicate removal, varToDelete.
-        $variables = @{}   # Dict{key==varName, value==varValue}
-        $_globals = @(Get-Content -Path $this.PATH_TO_GLOBALS)      # "@" added, Get-Content returns string when < 2 lines, making `$lines.AddRange($_globals)` throw an exception
+    # Also does cleanup while loading into memory, e.g. duplicate removal, varToDelete.
+    hidden [void] LoadInGlobals($varToDelete) {      
+        $variables = @{}                                             # Dict{key==varName, value==varValue}
+        $_globals  = @(Get-Content -Path $this.PATH_TO_GLOBALS)      # "@" added, Get-Content returns string when < 2 lines, making `$lines.AddRange($_globals)` throw an exception
         
         if(-not($_globals)) {  PrintRed "Globals Empty";  RETURN;  }
 
         Clear-Host
         $lines = [System.Collections.Generic.List[Object]]::new(); $lines.AddRange($_globals)
         for ($i = 0; $i -lt $lines.Count; $i++) {
-            $left = $lines[$i].Split("=")[0]
+            $left  = $lines[$i].Split("=")[0]
             $right = $lines[$i].Split("=")[1]
             if ($left -eq "" -or $right -eq "" -or $left -eq $varToDelete -or $variables.ContainsKey($left)) {    # is duplicate if $variables.containsKey($left)
                 $lines.RemoveAt($i)
@@ -103,7 +97,6 @@ class MyRuntime {
                 }
             }
             else {
-                
                 $variables.Add($left, $right)
                 Set-Variable -Name $left -Value $right -Scope Global
 
