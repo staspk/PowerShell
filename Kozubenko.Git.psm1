4f6aa0de03a770e1ec60fa43eb09166f257eabda
Stanislav Kozubenko <staspk@gmail.com>
2025-07-26 17:56:17 -0700

    RebaseFinish Temporarily Disabled

--- Kozubenko.Git.psm1
+++ Kozubenko.Git.psm1
@@ -5,13 +5,15 @@ class KozubenkoGit {
         return [FunctionRegistry]::new(
             "Kozubenko.Git",
             @(
+                "GitPage()                             -->   goes to remote.origin.url in the browser",
                 "GitStatus()                           -->   Clear-Host; git status"
                 "Push(`$commitMsg)                      -->   push to github repo. does not work with branches",
-                "GitUpdateSubmodules()                 -->   git submodule update --init --recursive --remote --force",
+                "GitUpdateSubmodules()                 -->   git submodule update --init --recursive --remote [--force]",
                 "GitLog(`$lines=4)                      -->   afterwards use: 'git show 06cb024'",
                 "GitUncommit()                         -->   redo your last pushed commit: git reset --mixed HEAD~1",
-                "GitPage()                             -->   goes to remote.origin.url in the browser",
-                "SquashCommits(`$commitMsg, `$n)         -->   (n = # of all commits being combined). force push included"
+                "SquashCommits(`$commitMsg, `$n)         -->   (n = # of all commits being combined). force push included",
+                "Rebase(`$commitsBack)                  -->   [dd -> cut line] [P -> paste] [squash -> merges into above commit]",
+                "RebaseFinish()                        -->   git push --force; git stash pop;"
             ));
     }
 }
@@ -27,7 +29,7 @@ function Push($commitMsg = "No Commit Message") {
 }
 
 function GitUpdateSubmodules {
-    git submodule update --init --recursive --remote --force
+    git submodule update --init --recursive --remote
 }
 
 function GitLog($lines=4) {
@@ -51,14 +53,20 @@ function GitPage($path = $PWD.Path) {
     if(TryOpenGithub $path) {  RETURN;  }
     while($true) {
         $path = [System.IO.Path]::GetDirectoryName($path)
-        if(-not($path)) {  PrintRed "No .git config file found with `$path: $path, or with any ancestor"; RETURN;  }
+        if(-not($path)) {  PrintRed ".git config file not found. not at `$path, not with any ancestor"; RETURN;  }
         if(TryOpenGithub $path) {  RETURN;  }
     }
 }
 
+<#
+PLEASE LOOK INTO REPLACING "git push --force" with "git push --force-with-lease":
+This may work, needs testing:
+    $output = git push --force-with-lease 2>&1
+    $exitCode = $LASTEXITCODE
+#>
 function SquashCommits($commitMsg, $n) {
-    AssertString $commitMsg "commitMsg"
-    if($n -lt 2) {  PrintRed "required: n > 1"; RETURN;  }
+    AssertString "commitMsg" $commitMsg 
+    if($n -lt 2) {  PrintRed "SquashCommits(`$commitMsg, `$n): Requirement not met: `$n > 1"; RETURN;  }
 
     $need_stash = (git status --porcelain)
     if($need_stash) {
@@ -73,6 +81,31 @@ function SquashCommits($commitMsg, $n) {
 
     if($need_stash) {
         git stash pop
+    }
+}
+
+function Rebase([int]$commitsBack) {
+    $global:rebaseStarted = $true
+
+    $global:stashed_on_rebase = (git status --porcelain)
+    if($global:stashed_on_rebase) {
+        git stash
+    }
+    git rebase -i HEAD~$($commitsBack)
+}
+function RebaseFinish() {
+    Write-Host
+    PrintDarkRed "RebaseFinish() temporarily disabled."
+    PrintRed "`$global:stashed_on_rebase: $global:stashed_on_rebase"
+    PrintDarkRed "git push --force will have to be done manually."
+    return;
+    if ($global:rebaseStarted -eq $true) {
+        git push --force
+
+        if($global:stashed_on_rebase) {  git stash pop  }
+        
+
+        $global:rebaseStarted = $false
     }
 }
 
