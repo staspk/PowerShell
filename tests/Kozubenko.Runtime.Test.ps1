a72376736a6ab58e5420b99c60a7e5cf7b03605c
Stanislav Kozubenko <staspk@gmail.com>
2025-08-26 20:37:17 -0700

    basic testing

--- tests/Kozubenko.Runtime.Test.ps1
+++ tests/Kozubenko.Runtime.Test.ps1
@@ -1,27 +1,45 @@
+<# 
+    Some Test Results:
+
+    $value: Union<string, array>
+    @($value)[0]  => coercion/getting works, and is simpler than checking/handling if $value is str or array. 
+        note: SafeCoerceToArray extends @() to handle $null, allowing for: Union<null, string, array>
+#>
+
 using module ..\Kozubenko.Utils.psm1
 using module ..\Kozubenko.Runtime.psm1
-
 
 
 $MyRuntime = [MyRuntime]::new($PSScriptRoot)
 Remove-Item $MyRuntime._GLOBALS_FILE
 Remove-Item $MyRuntime._COMMANDS_FILE
 
-
 $MyRuntime.NewCommand(
     "C:\Users\stasp\Documents\PowerShell",
     "Invoke-Pester -Path .\tests\Get-Content.Test.ps1 -Output Detailed"
-)
+) 2>&1 4>&1 5>&1 6>&1 > $null
 
 $MyRuntime.NewCommand(
     "C:\Users\stasp\Documents\PowerShell",
     "Invoke-Pester -Path Kozubenko.Runtime.Tests.ps1 -Output Detailed"
-)
+) 2>&1 4>&1 5>&1 6>&1 > $null
 
 $commands = [MyRuntime]::LoadEnvFileIntoMemory($MyRuntime._COMMANDS_FILE)
+$MyRuntime.commands = $commands
+
+$MyRuntime.CycleCommands()
+$MyRuntime.CycleCommands()
+$MyRuntime.CycleCommands()
+
+if($MyRuntime.history_depth -lt 3) {
+    PrintGreen "Decrement_HistoryDepth Test Passed" -NewLine
+} else {
+    PrintRed "Decrement_HistoryDepth Test Failed" -NewLine
+}
 
 
-Write-Host
+Remove-Item $MyRuntime._GLOBALS_FILE -Force 2>$null
+Remove-Item $MyRuntime._COMMANDS_FILE -Force 2>$null
 
 
 
@@ -29,26 +47,3 @@ Write-Host
 #     "C:\Users\stasp\Desktop\OS-Setup\Windows11\.vscode\vsc-augment",
 #     ".\compile_import_into_vscode.ps1"
 # )
-
-
-# $MyRuntime.NewCommand(
-#     "C:\Users\stasp\Documents\PowerShell",
-#     [string[]]@(
-#         "Invoke-Pester -Path .\Kozubenko.Utils.List.Test.ps1 -Output Detailed",
-#         "Invoke-Pester -Path .\Kozubenko.Utils.List.Tests.ps1 -Output Detailed"
-#     )
-# )
-
-
-Write-Host
-
-
-
-# $MyRuntime.NewCommand(
-#     "C:\Users\stasp\Documents\PowerShell",
-#     [string[]]@(
-#         "Invoke-Pester -Path .\tests\Get-Content.Test.ps1 -Output Detailed",
-#         "Invoke-Pester -Path .\tests\Get-Content.Tests.ps1 -Output Detailed"
-#     )
-# ) 6>&1 > $null
-
